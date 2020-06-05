fca352fd8bde07fa8580ab500ce3b950782a1897
Linus Frotscher <linusfrotscher@gmail.com>
2020-06-05 09:54:51 +0100

    update

---
+++ build/precache-manifest.c2ae810bf53ac138f49d54b1cc1e7346.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b5300ed23b3c7665c2f5a2dff4567a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc28c84d13fb5697907",
+    "url": "/static/css/main.8d722f03.chunk.css"
+  },
+  {
+    "revision": "7f731af6dbe9206e35a0",
+    "url": "/static/js/2.77c7765d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.77c7765d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc28c84d13fb5697907",
+    "url": "/static/js/main.05a1e6b4.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  },
+  {
+    "revision": "4d4b55fd254bd5aab4f981403344ffd2",
+    "url": "/static/media/kitesurf.4d4b55fd.JPG"
+  },
+  {
+    "revision": "c74103ced6e740405106ba6d55e246bb",
+    "url": "/static/media/labManager.c74103ce.PNG"
+  },
+  {
+    "revision": "b77be4284083c7a700242cacd30fd06f",
+    "url": "/static/media/labManager2.b77be428.PNG"
+  },
+  {
+    "revision": "ec8f783c98fa26845a465bbeaf1258af",
+    "url": "/static/media/labManager3.ec8f783c.PNG"
+  },
+  {
+    "revision": "5bfd96f6b05512b33cb6194ea619c7a8",
+    "url": "/static/media/labManager4.5bfd96f6.PNG"
+  },
+  {
+    "revision": "2e3f87237f5bbca9b488c3688d40ee92",
+    "url": "/static/media/linus.2e3f8723.jpg"
+  },
+  {
+    "revision": "a1b30710bd882ceec0cd416ca69e004d",
+    "url": "/static/media/logo.a1b30710.Bu9v87"
+  },
+  {
+    "revision": "0d8aed9664cf07709adade53aeed9793",
+    "url": "/static/media/prophy.0d8aed96.png"
+  },
+  {
+    "revision": "8c18937935de504f3a8123389abdd0e7",
+    "url": "/static/media/prophy2.8c189379.png"
+  },
+  {
+    "revision": "e1ebb67fada17e45c2a3899464b881ab",
+    "url": "/static/media/prophy3.e1ebb67f.png"
+  },
+  {
+    "revision": "7deb9cb17de72e60ba57dd64f99d9bf0",
+    "url": "/static/media/prophy4.7deb9cb1.png"
+  },
+  {
+    "revision": "e30a3e99b86b0337bdf5bb6a58174317",
+    "url": "/static/media/prophy5.e30a3e99.png"
+  }
+]);
